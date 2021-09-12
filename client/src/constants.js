c62f3eb56250bd1870f4126c0dd6fcea694cc09a
NoaBabliki <51087030+NoaBabliki@users.noreply.github.com>
2021-09-12 19:50:46 +0300

    Add files via upload

---
+++ client/src/constants.js
@@ -0,0 +1,6 @@
+export const CATEGORY_1 = "Concert tickets"
+export const CATEGORY_2 = "Workshops"
+//export const SHOW_OPTIONS_PAGE = 2 //number of times to show options page
+export const MAX_CATEGORY_DISPLAY = 40
+export const INSTRUCTIONS = "In this task, we ask you to rate your preferences in a given category. You will see different options in a given category. Please rate them one by one on a scale of 1-1000."
+export const NUM_CUSTOM_OPTIONS = 5 //number of custom options the participent can add
