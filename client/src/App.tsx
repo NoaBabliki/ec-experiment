d86a00a59abcad2db82e09057a4f5bbcea3a7092
NoaBabliki <51087030+NoaBabliki@users.noreply.github.com>
2021-10-18 11:20:19 +0300

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,94 +1,41 @@
 import React from 'react';
 import './App.css';
-import {createApiClient, createApiClientChoices} from './api'; //connection to server
+import {createApiClient} from './api'; //connection to server
+import ChooseOptions from './components/ChooseOptions';
 import StartExperiment from './components/StartExperiment';
-import * as constants from './constants.js'
-import FirstStageWrapper from './components/FirstStageWrapper';
-import SecondStageWrapper from './components/SecondStageWrapper';
-
-
-const FIRST_TITLE = 'Welcome'
-const SECOND_TITLE_PART_1 = 'What do you prefer?'
-const SECOND_TITLE_PART_2 = 'Make the two options equal'
-const INSTRUCTIONS_PART_1 = 'In this task, we ask you to rate your preferences in a given category. You will see different options in a given category. Please rate them one by one on a scale of 1-1000.'
-const INSTRUCTIONS_PART_2 = 'In this task, we ask you to compare between two options, each including a ' + constants.CATEGORY_1_SINGULAR + ' and a ' + constants.CATEGORY_2_SINGULAR + '. At each screen, you will see two options and their attributes, with one attribute value missing. Please type in an attribute value that will make the two options equally preferred, from the pools of the first task.'
-
-// for testing perpuses:
-const CATEGPRIES = [
-  [
-    {
-      id: 0,
-      name: "very long band name 0",
-      rating: 100
-    },
-    {
-      id: 1,
-      name: "long band name 1",
-      rating: 200
-    },
-    {
-      id: 2,
-      name: "long band name 2",
-      rating: 900
-    },
-    {
-      id: 3,
-      name: "long band name 3",
-      rating: 800
-    }
-  ],
-  [
-    {
-      id: 0,
-      name: "work0",
-      rating: 150
-    },
-    {
-      id: 1,
-      name: "work1",
-      rating: 250
-    },
-    {
-      id: 2,
-      name: "work2",
-      rating: 950
-    },
-    {
-      id: 3,
-      name: "work3",
-      rating: 850
-    }
-  ]
-]
-
 
 
 export type AppState = {
   flow: number,
   category_index: number,
-  max_id: number,
   client_catigories: object[][],
   categories?: object[][],
-  choices?: object[][]
 }
 
 const api = createApiClient()
-const apiChoices = createApiClientChoices()
 
 export class App extends React.PureComponent<{}, AppState> {
 
   state: AppState = {
-    flow: 0, //changed to 0 for text purpuses! change back to 1 when running
+		flow: 1,
     category_index: 0,
-    max_id: 0,
     client_catigories: [[],[]]
 	}
 
+  /** 
+  dataUpdate = async () => {
+    this.setState({
+			categories: await api.getProperties()
+		});
+    //console.log(this.state.categories)
+	}
+  */
+
   async componentDidMount() {
     this.setState({
-			categories: await api.getProperties(this.state.client_catigories),
+			categories: await api.getProperties(this.state.client_catigories)
 		});
-    this.setState({max_id: this.setMaxCategoryId()})
+    //console.log(this.state.categories)
   }
 
   //from strig array to json array ---> save client data as json
@@ -105,100 +52,44 @@ export class App extends React.PureComponent<{}, AppState> {
     return options_array
   }
 
-  setCategoryIndex = () => {
+  setCategories = async (newCategory: object[]) => {
+    var new_client_categories = this.state.client_catigories.slice(0)
+    new_client_categories[this.state.category_index] = newCategory
+    //console.log('client categories', new_client_categories)
     let add_to_index = 1
     if (this.state.category_index === add_to_index){
       add_to_index = -1
     }
     this.setState({
-      category_index: this.state.category_index + add_to_index
-    })
-  }
-
-  setCategories = async (newCategory: object[]) => {
-    var new_client_categories = this.state.client_catigories.slice(0)
-    new_client_categories[this.state.category_index] = newCategory
-    this.setState({
+      category_index: this.state.category_index + add_to_index,
       client_catigories: new_client_categories,
       categories: await api.getProperties(new_client_categories)
     })
+    //console.log('client categories after update', this.state.client_catigories)
   }
 
   setFlow = () => {
-    this.setState({
-      flow: this.state.flow + 1
-    })
+    this.setState({flow: this.state.flow + 1})
   }
-
-  addParticipentOptions = async (new_caterories: object[]) => {
-    var new_client_categories = this.state.client_catigories.slice(0)
-    new_client_categories[this.state.category_index] = new_client_categories[this.state.category_index].concat(new_caterories)
-    this.setState({
-      client_catigories: new_client_categories,
-      categories: await api.getProperties(new_client_categories)
-    })
-    console.log("categories after adding option:",this.state.categories)
-  }
-
-setMaxCategoryId(){
-  let max_id = constants.MAX_CATEGORY_DISPLAY
-  if (this.state.categories){
-    for (let category of this.state.categories){
-      let temp_id = category.length
-      if (temp_id > max_id){
-        max_id = temp_id
-      }
-    }
-  }
-  return max_id
-}
-
-setChoices = async (choices: object[][]) => {
-  console.log(choices)
-  this.setState({
-    choices: await apiChoices.getProperties(choices)
-  })
-}
 
   render() {
-    let title_2 = SECOND_TITLE_PART_1
-    let instructions = INSTRUCTIONS_PART_1
-    if (this.state.flow === 3){
-      title_2 = SECOND_TITLE_PART_2
-      instructions = INSTRUCTIONS_PART_2
-    }
     return( 
-    <main>
+    <main>   
       <div>
-        {(this.state.flow === 1 || this.state.flow === 3)?
+        {this.state.flow === 1?
         <StartExperiment 
-        first_title={FIRST_TITLE}
-        second_title={title_2}
-        instructions={instructions}
-        flow={this.state.flow}
         setFlow={this.setFlow}>
         </StartExperiment> : null}
       </div> 
-      <div>
-        {(this.state.flow === 2 && this.state.categories)?
-        <FirstStageWrapper
-        categories={this.state.categories}
-        client_categories={this.state.client_catigories}
-        max_id={this.state.max_id}
+      <div>  
+      {(this.state.categories && this.state.flow === 2)?
+      <ChooseOptions key={this.state.category_index} 
+        categories={this.state.categories} 
+        index={this.state.category_index}
         setCategories={this.setCategories}
-        addParticipentOptions={this.addParticipentOptions}
-        setCategoryIndex={this.setCategoryIndex}
         setFlow={this.setFlow}>
-        </FirstStageWrapper> : null}
+      </ChooseOptions> : null}
       </div>
-      <div>
-      {this.state.flow === 0? //changes to 0 for test perpuses!!! change back to 4 when running
-       <SecondStageWrapper
-       categories={CATEGPRIES}
-       setChoices={this.setChoices}>
-       </SecondStageWrapper>
-       :null}
-      </div>   
     </main>
     )
   }
